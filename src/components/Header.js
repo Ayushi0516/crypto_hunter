c8daf9d89d330a58d9c1b50ce606b52252c62509
Ayushi0516 <ayushigupta0516@gmail.com>
2023-06-06 23:02:24 +0530

    routing added

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+import React from "react";
+
+const useStyles=makeStyles(()=>({
+    title:{
+        flex:1,
+        color:"gold",
+        fontFamily:"Montserrat",
+        fontWeight:"bold",
+        cursor:"pointer"
+    },
+}))
+
+const Header = () => {
+    const classes=useStyles();
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography className={classes.title}>Crypto Hunter</Typography>
+          <Select variant="outlined" style={{width:100,height:40, marginLeft:15}}>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"INR"}>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
