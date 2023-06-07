a693a3fa8a977c6176fe6fc0652a4dfc10d4257a
Ayushi0516 <ayushigupta0516@gmail.com>
2023-06-07 22:53:26 +0530

    context api implemented

---
+++ src/CryptoContext.js
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react'
+
+const Crypto =createContext();
+
+const CryptoContext =({children})  => {
+  return (
+    <Crypto.Provider>
+      {children}
+    </Crypto.Provider>
+    
+   
+  )
+}
+
+export default CryptoContext
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,45 @@
 import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 import React from "react";
+import { useNavigate} from "react-router-dom"
 
 const useStyles=makeStyles(()=>({
     title:{
         flex:1,
         color:"gold",
         fontFamily:"Montserrat",
-        fontWeight:"bold",
+        fontWeight:"900",
         cursor:"pointer"
     },
 }))
 
 const Header = () => {
     const classes=useStyles();
+    const navigate=useNavigate();
+
+    const darkTheme=createTheme({
+      palette:{
+        primary:{
+          main:"#fff",
+        },
+        mode:"dark",
+      }
+    })
   return (
+    <ThemeProvider theme={darkTheme}>
     <AppBar color="transparent" position="static">
       <Container>
         <Toolbar>
-          <Typography className={classes.title}>Crypto Hunter</Typography>
-          <Select variant="outlined" style={{width:100,height:40, marginLeft:15}}>
+          <Typography variant="h6" onClick={()=>navigate("/")} className={classes.title}>Crypto Hunter</Typography>
+          <Select variant="outlined" style={{width:100,height:40, marginRight:15}}>
             <MenuItem value={"USD"}>USD</MenuItem>
             <MenuItem value={"INR"}>INR</MenuItem>
           </Select>
         </Toolbar>
       </Container>
     </AppBar>
+    </ThemeProvider>
   );
 };
 
