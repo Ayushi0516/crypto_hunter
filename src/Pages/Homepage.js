1b782956d8f04cfdcef6e83e1ac82a1dc6462113
Ayushi0516 <ayushigupta0516@gmail.com>
2023-06-08 23:44:35 +0530

    coin data fetched

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Banner from '../components/Banner/Banner'
+import React from "react";
+import Banner from "../components/Banner/Banner";
+import CoinsTable from "../components/CoinsTable";
 
 const Homepage = () => {
   return (
     <div>
- <Banner/>
+      <Banner />
+      <CoinsTable />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
